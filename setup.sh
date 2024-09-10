06888f96ba83ab15a5cd3470c1e6bd9116e5cb0b
jonas-jaderberg <75827629+jonas-jaderberg@users.noreply.github.com>
2024-09-10 18:09:57 +0000

    version 7.8.0

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 
 echo 'Starting wget'
-sudo wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.0.0/openapi-generator-cli-6.0.0.jar -O /usr/local/bin/openapi-generator-cli.jar || { echo 'wget failed'; exit 1; }
+sudo wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.8.0/openapi-generator-cli-7.8.0.jar -O /usr/local/bin/openapi-generator-cli.jar || { echo 'wget failed'; exit 1; }
 
 echo 'Starting chmod'
 sudo chmod +x /usr/local/bin/openapi-generator-cli.jar || { echo 'chmod failed'; exit 1; }
