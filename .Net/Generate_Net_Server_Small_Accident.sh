bda66d6efc9b5bbdffefd7d6301b9660078ef083
jonas-jaderberg <75827629+jonas-jaderberg@users.noreply.github.com>
2024-09-12 09:55:46 +0000

    Fix .Net

--- .Net/Generate_Net_Server_Small_Accident.sh
+++ .Net/Generate_Net_Server_Small_Accident.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar /usr/local/bin/openapi-generator-cli.jar generate -i ../Schema_Small_Accident/datexpull-1.0.2.yaml -g  aspnetcore -o .Net/DatexServer_Small_Accident --additional-properties=aspnetCoreVersion=6.0
+java -jar /usr/local/bin/openapi-generator-cli.jar generate -i ../Schema_Small_Accident/datexpull-1.0.2.yaml -g  aspnetcore -o .Net/DatexServer_Small_Accident --additional-properties=aspnetCoreVersion=8.0
