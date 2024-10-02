00b79a0914075965ee6e6edff6ad0d9e3e8ddf73
jonas-jaderberg <75827629+jonas-jaderberg@users.noreply.github.com>
2024-10-02 08:27:23 +0000

    add skip-operation-example

--- Python/Generate_Python_Server_Small_Accident.sh
+++ Python/Generate_Python_Server_Small_Accident.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-java -jar /usr/local/bin/openapi-generator-cli.jar generate -i generate -i ../Schema_Small_Accident/datexpull-1.0.2.yaml -g  python-flask -o Python/DatexServer_Small_Accident --skip-operation-example
+java -jar /usr/local/bin/openapi-generator-cli.jar generate \
+    -i ../Schema_Small_Accident/datexpull-1.0.2.yaml \
+    -g  python-flask \
+    -o Python/DatexServer_Small_Accident \
+    --skip-operation-example
