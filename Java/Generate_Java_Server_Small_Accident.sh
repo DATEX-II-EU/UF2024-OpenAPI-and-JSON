0a0d73b6b100d95db3724593d9f9f8e76d14883d
jonas-jaderberg <75827629+jonas-jaderberg@users.noreply.github.com>
2024-10-02 08:23:37 +0000

    Cleanup Java generation file

--- Java/Generate_Java_Server_Small_Accident.sh
+++ Java/Generate_Java_Server_Small_Accident.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-java -jar /usr/local/bin/openapi-generator-cli.jar generate -i generate -i ../Schema_Small_Accident/datexpull-1.0.2.yaml -g spring -o Java/DatexServer_Small_Accident
+java -jar /usr/local/bin/openapi-generator-cli.jar generate \
+    -i ../Schema_Small_Accident/datexpull-1.0.2.yaml \
+    -g spring \
+    -o Java/DatexServer_Small_Accident
