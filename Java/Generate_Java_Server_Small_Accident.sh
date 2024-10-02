396c7721f14473ad5dce582a2a173d3c6a204ce6
jonas-jaderberg <75827629+jonas-jaderberg@users.noreply.github.com>
2024-10-02 08:31:15 +0000

    Cleanupp removed all generated files

--- Java/Generate_Java_Server_Small_Accident.sh
+++ Java/Generate_Java_Server_Small_Accident.sh
@@ -2,5 +2,5 @@
 java -jar /usr/local/bin/openapi-generator-cli.jar generate \
     -i ../Schema_Small_Accident/datexpull-1.0.2.yaml \
     -g spring \
-    -o Java/DatexServer_Small_Accident \
+    -o DatexServer_Small_Accident \
     --skip-operation-example
